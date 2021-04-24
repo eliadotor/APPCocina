4bd7464647a5d409b6977656f8861269bb674f4b
Elia Dotor Puente <elia.dotor@gmail.com>
2021-04-24 20:00:44 +0200

    Initial Commit

---
+++ Shared/APPCocinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPCocinaApp.swift
+//  Shared
+//
+//  Created by Elia Dotor Puente on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct APPCocinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
