12cf1c4ed7bf063d5265a37b65f349166f4a2931
Elia Dotor Puente <elia.dotor@gmail.com>
2021-05-20 16:10:16 +0200

    Create ValidacionFormularioView y EstiloBotonView

---
+++ Shared/View/EstiloBotonView.swift
@@ -0,0 +1,53 @@
+//
+//  EstiloBotonView.swift
+//  cocina
+//
+//  Created by Elia Dotor Puente on 20/05/2021.
+//
+
+import SwiftUI
+
+struct EstiloBotonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EstiloBotonCrearReceta: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .padding()
+        .frame(height: 120)
+        .background(Color.yellow)
+        .cornerRadius(12)
+    }
+}
+
+struct EstiloBoton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .font(.title2)
+        .foregroundColor(.white)
+        .frame(height: 45)
+        .background(Color.orange)
+        .cornerRadius(10)
+        .padding()
+    }
+}
+
+
+struct EstiloBotonDesactivado: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .font(.title2)
+        .padding(.vertical)
+        .foregroundColor(.yellow)
+        .frame(height: 45)
+        .background(Color.yellow.opacity(0.3))
+        .cornerRadius(10)
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.yellow, lineWidth: 1).frame(height: 45))
+    }
+}
---
+++ Shared/View/ValidacionFormularioView.swift
@@ -0,0 +1,56 @@
+//
+//  ValidacionFormularioView.swift
+//  cocina
+//
+//  Created by Elia Dotor Puente on 20/05/2021.
+//
+
+import SwiftUI
+
+/* Estructura para validar un campo de un formulario */
+struct ValidacionFormularioView: View {
+    var nombreIcono = "xmarc.circle"
+    var colorIcono = Color(red: 0.9, green: 0.5, blue: 0.5)
+    var texto = ""
+    var body: some View {
+        HStack {
+            Image(systemName: nombreIcono)
+                .foregroundColor(colorIcono)
+            Text(texto)
+                .foregroundColor(.gray)
+            Spacer()
+        }
+    }
+}
+
+/* Estructura para crear un campo de un formulario,
+   puede ser de tipo TextField o SecureField */
+struct SingleFormView: View {
+    var nombreCampo = ""
+    @Binding var valorCampo: String
+    var protegido = false
+    
+    var body: some View {
+        VStack {
+            if protegido {
+                SecureField(nombreCampo, text: $valorCampo)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding(.horizontal)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 1).frame(height: 40))
+                    .padding(.vertical)
+            } else {
+                TextField(nombreCampo, text: $valorCampo)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .keyboardType(.emailAddress)
+                    .padding(.horizontal)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 1).frame(height: 40))
+                    .padding(.vertical)
+            }
+        }
+        
+    }
+}
+
+
