b8a2fb28f214b0869ae48a21e5765c698d73c2ea
Elia Dotor Puente <elia.dotor@gmail.com>
2021-05-21 15:39:10 +0200

    Create RecetasView

---
+++ Shared/View/RecetasView.swift
@@ -0,0 +1,21 @@
+//
+//  RecetasView.swift
+//  APPCocina
+//
+//  Created by Elia Dotor Puente on 21/05/2021.
+//
+
+import SwiftUI
+
+struct RecetasView: View {
+    var irARecetas: Binding<Bool>
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+/*struct RecetasView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecetasView(irARecetas: irRecetas)
+    }
+}*/
